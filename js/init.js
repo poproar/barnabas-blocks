ffe0e5a72df4fba3174b6496018d967231cb36d6
Andrew <andrew@barnabasrobotics.com>
2020-06-24 11:44:23 -0700

    enable web serial notice

--- js/init.js
+++ js/init.js
@@ -299,6 +299,18 @@ function getParam() {
 }
 
 function setScript() {
+
+  if (!('serial' in navigator)) {
+    $('#notSupported').openModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      // ready: function() { alert('Ready'); }, // Callback for Modal open
+      // complete: function() { alert('Closed'); } // Callback for Modal close
+    });
+  }
+
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.id = 'msg';
