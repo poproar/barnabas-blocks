09e572282f426b1e9f468e4268365ca59c76f884
Andrew <andrew@barnabasrobotics.com>
2020-07-31 14:18:50 -0700

    add if block

--- generators/arduino/control.js
+++ generators/arduino/control.js
@@ -43,10 +43,31 @@ Blockly.Arduino['controls_delay'] = function (block) {
 
 Blockly.Arduino['controls_loop'] = function (block) {
   let statements_loop = Blockly.Arduino.statementToCode(block, 'LOOP');
-
   Blockly.Arduino.loop_ = statements_loop;
   return '';
 }
+
+Blockly.Arduino['controls_if'] = function() {
+  // If/elseif/else condition.
+  var n = 0;
+  var argument = Blockly.Arduino.valueToCode(this, 'IF' + n,
+      Blockly.Arduino.ORDER_NONE);
+  var branch = Blockly.Arduino.statementToCode(this, 'DO' + n);
+  // console.log(argument);
+  // if (argument === 'undefined' || argument === '') return Blockly.alert("Missing Test");
+  var code = 'if (' + argument + ') {\n' + branch + '\n}';
+  for (n = 1; n <= this.elseifCount_; n++) {
+    argument = Blockly.Arduino.valueToCode(this, 'IF' + n,
+      Blockly.Arduino.ORDER_NONE) || 'false';
+    branch = Blockly.Arduino.statementToCode(this, 'DO' + n);
+    code += ' else if (' + argument + ') {\n' + branch + '}';
+  }
+  if (this.elseCount_) {
+    branch = Blockly.Arduino.statementToCode(this, 'ELSE');
+    code += ' else {\n' + branch + '\n}';
+  }
+  return code + '\n';
+};
 
 Blockly.Arduino['boolean_onoff'] = function (block) {
   // Boolean values HIGH and LOW.
